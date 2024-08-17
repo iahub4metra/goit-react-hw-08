883574bb1fe0ad47343742b69bb1d556091f71a2
iahub4metra <buhai.artem7@gmail.com>
2024-08-17 20:34:58 +0100

    homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,16 @@
+import { Button } from "@mui/material";
+import s from "./HomePage.module.css"
+import { NavLink } from "react-router-dom";
 const HomePage = () => {
     return (
         <>
-            <p>This is homepage</p>
+            <div className={s.homeContainer}>
+                <div>
+                    <h1 className={s.mainTitle}>PhoneBook</h1>
+                    <p>Keep your connections close and your contacts closer—manage them all in one place with ease.</p>
+                    <Button variant="contained"><NavLink className={s.homeNavLink} to="/register">Get Started</NavLink></Button>
+                </div>
+            </div>
         </>
     );
 }
